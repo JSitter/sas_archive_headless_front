4c4814aab34ba2029396f9e5890f67d0d1da5a96
Justin Sitter <justin@jaytria.com>
2020-09-21 16:20:31 -0700

    Add elegant styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './css/base-styles.css';
+import dockerBrands from './assets/dockerBrands.svg';
 
 function App() {
 
     return (
-        <div className="App-header">
-            <p>This is a great Reaction App!</p>
+        <div className="application">
+            <div className="header">
+            <a href="https://fontawesome.com/license/free">
+                <img className="docker-logo" src={dockerBrands} />
+            </a>
+            <h1>Simple Dockerized React App</h1>
+            </div>
+            <p>
+                This repo is an example of a simple React app the builds into a folder that can be hosted on any normal hosting provider.
+            </p>
+            <p>
+                Simply copy the contents of the dist folder to your server to host your single page application on a regular LAMP stack.
+            </p>
         </div>
     );
 }
