458de8cede608474d18aeed66e2e43d8a0146f9e
Justin Sitter <justin@jaytria.com>
2020-09-21 14:14:22 -0700

    Add dependencies

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+
+    return (
+        <div className="App-header">
+            <p>This is a great React App!</p>
+        </div>
+    );
+}
+
+export default App;
