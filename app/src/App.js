ae9b14d5b58672691a555ebc4d38c04e5dd5cce9
Justin Sitter <justin@jaytria.com>
2020-09-21 15:36:12 -0700

    Add css styles

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './css/base-styles.css';
 
 function App() {
 
     return (
         <div className="App-header">
-            <p>This is a great React App!</p>
+            <p>This is a great Reaction App!</p>
         </div>
     );
 }
