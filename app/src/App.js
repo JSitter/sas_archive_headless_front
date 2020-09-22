eb2c1c44b44c3136e23e0acc96877c427f25817c
Justin Sitter <justin@jaytria.com>
2020-09-22 15:45:10 -0700

    Add Header and MainMenu

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import './css/base-styles.css';
-import dockerBrands from './assets/dockerBrands.svg';
+import './css/base.css';
+import Header from './components/Header.jsx';
+import MainMenu from './components/MainMenu.jsx';
 
 function App() {
 
     return (
         <div className="application">
-            <div className="header">
-            <a href="https://fontawesome.com/license/free">
-                <img className="docker-logo" src={dockerBrands} />
-            </a>
-            <h1>Simple Dockerized React App</h1>
-            </div>
-            <p>
-                This repo is an example of a simple React app the builds into a folder that can be hosted on any normal hosting provider.
-            </p>
-            <p>
-                Simply copy the contents of the dist folder to your server to host your single page application on a regular LAMP stack.
-            </p>
+            <Header />
+            <MainMenu />
+
         </div>
     );
 }
