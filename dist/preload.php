01c0d2c17af077e7a39a3e7d36dc31330d27234f
Justin Sitter <justin@jaytria.com>
2020-09-26 14:18:32 -0700

    Tweak layout

--- dist/preload.php
+++ dist/preload.php
@@ -1,18 +1,19 @@
 <?php
     $preloadHeaderColor = '#596478';
-    $headerSize = '450px';
-    $menuSize = "50px";
+    $headerSize = '550px';
+    $menuSize = "80px";
     $menuColor = "#333";
     $headerBackgroundPosition = "calc(100vw / -10)";
     $backgroundBlur = "10px";
     $menuBoxShadow = "2px 5px 5px rgba(30, 30, 30, .3)";
+    $menuItemWidth = "130px";
 ?>
 <div id="app-body" style="border:0px;margin:0px;padding:0px;">
 <header style="width:100%;height:<?php print($headerSize) ?>;background-color:<?php print($preloadHeaderColor) ?>;overflow:hidden;">
     <img src="./assets/seminarycovertiny.jpg" style="width:100%;height:auto;filter:blur(<?php print($backgroundBlur) ?>);margin-top:<?php print($headerBackgroundPosition) ?>;"/>
 </header>
 <nav style="background-color:<?php print($menuColor) ?>;width:100%;height:<?php print($menuSize) ?>;display:flex;flex-direction:row;box-shadow:0px 3px 5px rgba(30, 30, 30, .6);">
-    <?php print(str_repeat('<div style="background-color:#555;height:100%;width: 75px;margin: 0px 10px;"></div>', 3)) ?>
+    <?php print(str_repeat('<div style="background-color:#555;height:100%;width:'.$menuItemWidth.';margin: 0px 10px;"></div>', 3)) ?>
 </nav>
 <div style="width:100%;height:100%;display:flex;flex-direction:column;align-items:center;margin:50px;">
     <div style="width:80%;box-shadow:<?php print($menuBoxShadow) ?>; background-color: #fefefe;margin: auto 50px;border-radius:10px;display:flex;flex-direction:column;align-items:center;padding:90px;margin:50px;">
