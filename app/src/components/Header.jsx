27d4dd8bad38a934b584ecf899b556cf1ed007fa
Justin Sitter <justin@jaytria.com>
2020-09-27 15:34:39 -0700

    Add inital dropdown menu

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import seminarycover from "../assets/seminarycover50.jpg";
 
 export default function Header() {
+    let screenWidth = window.innerWidth;
     return (
         <section className="main-header white-links">
-            <h1>Saint Anthony's Seminary Archive</h1>
-            <p>1896-1987</p>
+            {/* <img className="header-hero" src={seminarycover} /> */}
+            <section className="header-text">
+                <h1>Saint Anthony's Seminary Archive</h1>
+                <p>1896-1987</p>
+                <p>{screenWidth}</p>
+            </section>
         </section>
     );
 }
