44f688c78dd69aa268e27ce66bce3ee0a77fe561
Justin Sitter <justin@jaytria.com>
2020-09-27 15:32:20 -0700

    Add basic Content component

---
+++ app/src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoadingTeaserContent from './LoadingTeaserContent.jsx';
+
+export default function Content() {
+    return (
+        <section className="main-content">
+            <LoadingTeaserContent />
+        </section>
+    )
+}
