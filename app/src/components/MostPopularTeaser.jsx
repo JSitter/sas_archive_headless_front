fed7f12d7c89724f85c3fe336a369c0e712ab09b
Justin Sitter <justin@jaytria.com>
2020-10-03 23:07:44 -0700

    Add teaser component

---
+++ app/src/components/MostPopularTeaser.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+export default function MostPopularTeaser() {
+    const api_address = "http://localhost:8085/api/node";
+
+    useEffect(()=>{
+        
+    }, []);
+    return (
+        <article>
+            Hello World!
+        </article>
+    );
+}
