ae8461f1c1009b5d49b736922025a7c8338f858e
Justin Sitter <justin@jaytria.com>
2020-10-03 23:07:10 -0700

    Add Suspense block

--- app/src/components/Content.jsx
+++ app/src/components/Content.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {lazy, Suspense } from 'react';
 import LoadingTeaserContent from './LoadingTeaserContent.jsx';
+
+const MostPopularTeaser = lazy(() => import('./MostPopularTeaser.jsx'));
 
 export default function Content() {
     return (
         <section className="main-content">
-            <LoadingTeaserContent />
+            <Suspense fallback={LoadingTeaserContent}>
+                <MostPopularTeaser />
+            </Suspense>
         </section>
     )
 }
